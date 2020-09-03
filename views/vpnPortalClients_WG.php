afa35f21fc9fba64e7eec10a3a0c87548fa70910
Nick Aquina <nickaquina@gmail.com>
2020-09-03 16:42:57 +0200

    Use valid HTML in WireGuard client view

--- views/vpnPortalClients_WG.php
+++ views/vpnPortalClients_WG.php
@@ -1,40 +1,68 @@
 <?php $this->layout('base', ['activeItem' => 'configs', 'pageTitle' => $this->t('configs')]); ?>
 <?php $this->start('content'); ?>
 
-<h2>You have <?=$this->e(count($wgclients))?> users </h2>
+<h2>You have <?= $this->e(count($wgclients)); ?> users </h2>
 
-<?php $count = 0?>
+<?php $count = 0; ?>
 <ul class="profileList">
-<?php foreach ($wgclients as $wg): ?>
-    <li><details>
-        <summary><?=$this->e($wg['name']); ?></summary>
-    <table class="tbl">
-    <tbody>
-    <?php $count++?>
-        <tr>
-        <th># </th>
-        <td><li><?=$this->e($count)?></li></td></tr>
-        <tr><th>Public Key: </th>
-        <td><li><?=$this->e($wg['public_key']) ?></li></td></tr>
-        <tr><th>IP: </th>
-        <td><li><?=$this->e($wg['ip']) ?></li></td></tr>
-       <tr> <th>Create at: </th>
-        <td><li><?=$this->e($wg['created']) ?></li></td></tr>
-     <tr>   <th>Last Modified: </th>
-        <td><li><?=$this->e($wg['modified']) ?></li></td></tr>
-        <tr><th>Info: </th>
-        <td><li><?=$this->e($wg['info']) ?></li></td></tr>
-        <tr><th>QR Code</th>
-            <!-- todo: use https if current page loaded with https -->
-        <td><img src="<?="http://" . $this->e($userlink); ?>/<?=$this->e($count); ?>?format=qrcode" style="width:250px;height:250px;"></td></tr>
-        <td><a href="<?="http://" . $this->e($userlink); ?>/<?=$this->e($count); ?>?format=config" title="<?=$this->e($wg['name']); ?>"><?=$this->etr('Download config', 50); ?></a></td>
-
-    </tbody>
-    </table>
-    </details>
-    </li>
-<?php endforeach; ?>
+    <?php foreach ($wgclients as $wg): ?>
+        <li>
+            <details>
+                <summary><?= $this->e($wg['name']); ?></summary>
+                <table class="tbl">
+                    <tbody>
+                    <?php ++$count; ?>
+                    <tr>
+                        <th>#</th>
+                        <td>
+                            <?= $this->e($count); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Public Key:</th>
+                        <td>
+                            <?= $this->e($wg['public_key']); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>IP:</th>
+                        <td>
+                            <?= $this->e($wg['ip']); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Create at:</th>
+                        <td>
+                            <?= $this->e($wg['created']); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Last Modified:</th>
+                        <td>
+                            <?= $this->e($wg['modified']); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Info:</th>
+                        <td>
+                            <?= $this->e($wg['info']); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Configuration</th>
+                        <!-- todo: use https if current page loaded with https -->
+                        <td><img src="<?= 'http://'.$this->e($userlink); ?>/<?= $this->e($count); ?>?format=qrcode"
+                                 style="width:250px;height:250px;" alt="QR Code with WireGuard configuration">
+                            <br>
+                            <!-- todo: use https if current page loaded with https -->
+                            <a href="<?= 'http://'.$this->e($userlink); ?>/<?= $this->e($count); ?>?format=config"
+                               title="<?= $this->e($wg['name']); ?>"><?= $this->etr('Download config', 50); ?></a></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </details>
+        </li>
+    <?php endforeach; ?>
 </ul>
-
 
 <?php $this->stop('content'); ?>
