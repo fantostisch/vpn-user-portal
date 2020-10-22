8162555950d9006d5bbe3c206bcb323700f946ce
Nick Aquina <nickaquina@gmail.com>
2020-10-22 11:45:53 +0200

    Run psalm before php-cs-fixer

--- check.sh
+++ check.sh
@@ -2,10 +2,10 @@
 set -e
 set -x
 
+psalm
 diff_highlight="/usr/share/doc/git/contrib/diff-highlight/diff-highlight"
 php-cs-fixer fix --dry-run --diff --diff-format udiff | "$diff_highlight" | colordiff
 php-cs-fixer fix --dry-run -q --stop-on-violation
-psalm
 vendor/bin/phpunit
 echo "Success"
 
